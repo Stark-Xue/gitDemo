95f464d8414ce3afe9e4c8209768f234e671cb50
73 <8606399+seth_xue@user.noreply.gitee.com>
2023-06-14 19:36:29 +0800

    修复非洲专区的功能

--- three.py
+++ three.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 def index(request):
 	return render(request, 'index.html')
 def africa(request):
-	return HttpResponse("非洲专区")
+	return HttpResponse("非洲专区888")
